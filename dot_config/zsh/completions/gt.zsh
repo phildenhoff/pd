e09bff162663d7901d42337b1b0706be47707f2b
Phil Denhoff <phil@denhoff.ca>
2022-03-15 23:53:13 +0000

    Support gt in other places

--- dot_config/zsh/completions/gt.zsh
+++ dot_config/zsh/completions/gt.zsh
@@ -11,7 +11,7 @@ _gt_yargs_completions()
   local reply
   local si=$IFS
   IFS=$'
-' reply=($(COMP_CWORD="$((CURRENT-1))" COMP_LINE="$BUFFER" COMP_POINT="$CURSOR" /opt/homebrew/bin/gt --get-yargs-completions "${words[@]}"))
+' reply=($(COMP_CWORD="$((CURRENT-1))" COMP_LINE="$BUFFER" COMP_POINT="$CURSOR" gt --get-yargs-completions "${words[@]}"))
   IFS=$si
   _describe 'values' reply
 }
